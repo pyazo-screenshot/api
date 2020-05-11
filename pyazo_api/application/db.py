e63b0e3a9d91ff31df168954268029ebb83ee452
Pavle Portic <pavle.portic@tilda.center>
2020-05-11 23:19:01 +0200

    Increase database connection pool to 20+20

--- pyazo_api/application/db.py
+++ pyazo_api/application/db.py
@@ -1,17 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from pyazo_api.config import config
-import logging
 
 SQLALCHEMY_DATABASE_URL = config.db.SQLALCHEMY_DATABASE_URI
 LOG_LEVEL = config.db.LOG_LEVEL
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(LOG_LEVEL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_size=20,
+    max_overflow=20,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
